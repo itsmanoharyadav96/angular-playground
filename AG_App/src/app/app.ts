a132108800653cc420182263c84e86aa2f61d4bf
Manohar Yadav <yadavmanohar78094@gmail.com>
2025-11-12 12:39:09 +0530

    define variable and properties

--- AG_App/src/app/app.ts
+++ AG_App/src/app/app.ts
@@ -12,6 +12,8 @@ import { Signup } from './signup/signup';
 export class App {
   protected readonly title = signal('AG_App');
   name:string = 'Manohar Yadav';
+  // data:String | number | boolean = 'Angular 16';
+  data:any = 'Angular 16';
 
   updateName(){
     this.name='Hitesh Yadav';
